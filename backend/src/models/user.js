128254e11a785ab78d9992ddcd9153b554476242
Dane Borg <bogdan.cirstea.en@gmail.com>
2020-11-08 16:54:36 +0000

    Updated

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const userSchema = mongoose.Schema({
     passwordHash: { type: String, required: true },
     email: { type: String, required: true, unique: true },
